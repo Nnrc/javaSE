da70729a07435cc91a4fe0abe557d2c98ad27c56
gaoyan <gaoyan@51signing.com>
2019-06-13 10:35:29 +0800

    1

---
+++ src/main/java/com/gy/node/NodeFunction.java
@@ -0,0 +1,20 @@
+package com.gy.node;
+
+/**
+ * @ClassName : NodeFunction
+ * @Auther : gy
+ * @Date : 2019/6/12 14:57
+ * @Description :   链表方法测试
+ */
+public class NodeFunction {
+    //初始化一个节点
+
+    public static void main(String[] args) {
+        Node node = new Node(1);
+        node.addData(2);
+        node.addData(3);
+        node.insert(3,0);
+        node.traverse();
+    }
+
+}
